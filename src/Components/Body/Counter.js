fd61634e2d05a51d45dd246b4470087c73d10c31
lordish <gitonga403@gmail.com>
2023-05-04 13:41:40 +0300

    Final commit

--- src/Components/Body/Counter.js
+++ src/Components/Body/Counter.js
@@ -1,7 +1,38 @@
+import React from "react";
 import './Counter.css'
+
+const counterData = [
+    {
+        number: '2k',
+       text: 'Clients' 
+    },
+    {
+        number: 400,
+       text: 'Running Projects' 
+    },
+    {
+        number: 800,
+       text: 'Projects Completed' 
+    },
+]
+
 function Counter() {
     return (
-        <div></div>
+                <section className="counter" id="projects">
+            <div className="container">
+                <div className="counter_wrapper">
+                    {
+                        counterData.map((item, index) => (
+                           <div className="counter_item" key={index}>
+                                <h3 className="counter_number">{item.number}+</h3>
+                                <h4 className="counter_title">{item.text}</h4>
+                    </div>
+                       )) 
+                    }
+                    
+                </div>
+            </div>
+        </section>
     )
 }
 export default Counter;
