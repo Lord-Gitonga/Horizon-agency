f3576275db1dd5a895487bbc7d236eaf5460bf8d
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-05-04 12:11:05 +0300

    add footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,40 @@
 import './footer.css'
 function Footer() {
     return (
-        <div></div>
+        <div>
+            <footer id="footer">
+      <div id="footer-main">
+        <div className="footer-col-main">
+          <h2>Horizon Agency</h2>
+          <h3>Grow with us</h3>
+          <p>
+            Elit ut consequat reprehenderit cupidatat nulla eu dolor nostrud do
+            est cillum in elit amet. Aliquip velit exercitation nulla labore
+            esse consectetur culpa exercitation adipisicing Lorem. Dolor aliqua
+            culpa eu officia.
+          </p>
+        </div>
+        <div className="footer-col">
+          <h3>Email</h3>
+          <h4>hamza@gmail.com</h4>
+          <h4>morris@gmail.com</h4>
+          <h4>wycliffe@gmail.com</h4>
+        </div>
+        <div className="footer-col">
+          <h3>Github</h3>
+          <h4>https://github.com/morris</h4>
+          <h4>https://github.com/wycliffe</h4>
+          <h4>https://github.com/hamza</h4>
+        </div>
+        <div className="footer-col">
+          <h3>Contacts</h3>
+          <h4>0743563243</h4>
+          <h4>0798654104</h4>
+          <h4>0776545809</h4>
+        </div>
+      </div>
+    </footer>
+        </div>
     )
 }
 export default Footer;
